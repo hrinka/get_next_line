e12b7833d5283b82aaedda295a90605fba1c9d29
Homma Rinka <hrinka@c1r10s1.42tokyo.jp>
2023-03-11 19:49:29 +0900

    3/11completed

--- get_next_line.c
+++ get_next_line.c
@@ -3,18 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hrinka <hrinka@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: hrinka <hrinka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/07 17:18:16 by hrinka            #+#    #+#             */
-/*   Updated: 2023/03/03 13:53:23 by hrinka           ###   ########.fr       */
+/*   Created: 2023/03/04 20:19:18 by hrinka            #+#    #+#             */
+/*   Updated: 2023/03/11 19:45:00 by hrinka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*free_helper(char *save, char *buf)
+{
+	free(save);
+	save = NULL;
+	free(buf);
+	buf = NULL;
+	return (NULL);
+}
+
+char	*ft_read(int fd, char *save)
+{
+	char	*buf;
+	int		read_byte;
+	char	*tmp;
+
+	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buf)
+		return (NULL);
+	read_byte = 1;
+	while (!ft_strchr(save, '\n') && read_byte != 0)
+	{
+		read_byte = read(fd, buf, BUFFER_SIZE);
+		if (read_byte < 0)
+			return (free_helper(save, buf));
+		buf[read_byte] = '\0';
+		tmp = ft_strjoin(save, buf);
+		if (tmp == NULL)
+			return (free_helper(save, buf));
+		free(save);
+		save = tmp;
+	}
+	free (buf);
+	return (save);
+}
+
 char	*ft_get_line(char *save)
 {
-	int		i;
+	size_t	i;
 	char	*s;
 
 	i = 0;
@@ -24,7 +59,6 @@ char	*ft_get_line(char *save)
 		i++;
 	s = (char *)malloc(sizeof(char) * (i + 2));
 	if (!s)
-		free (save); //addedd
 		return (NULL);
 	i = 0;
 	while (save[i] && save[i] != '\n')
@@ -58,7 +92,7 @@ char	*ft_save(char *save)
 	s = (char *)malloc(sizeof(char) * (ft_strlen(save) - i + 1));
 	if (!s)
 	{
-		free (save); //added
+		free (save);
 		return (NULL);
 	}
 	i += 1;
@@ -68,34 +102,6 @@ char	*ft_save(char *save)
 	s[c] = '\0';
 	free (save);
 	return (s);
-}
-
-char	*ft_read(int fd, char *save)
-{
-	char	*buf;
-	int		read_byte;
-	char	*tmp;
-
-	buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buf)
-		return (NULL);
-	read_byte = 1;
-	while (!ft_strchr(save, '\n') && read_byte != 0)
-	{
-		read_byte = read(fd, buf, BUFFER_SIZE);
-		if (read_byte == -1)
-		{
-			free (buf);
-			free (save);//added
-			return (NULL);
-		}
-		buf[read_byte] = '\0';
-		tmp = ft_strjoin(save, buf);
-		free(save);
-		save = tmp;
-	}
-	free (buf);
-	return (save);
 }
 
 char	*get_next_line(int fd)
@@ -110,30 +116,35 @@ char	*get_next_line(int fd)
 		return (NULL);
 	line = ft_get_line(save);
 	if (line == NULL)
+	{
+		free(save);
+		save = NULL;
 		return (NULL);
+	}
 	save = ft_save(save);
 	return (line);
 }
 
+// #include <stdio.h>
+// #include <fcntl.h>
 
-#include <stdio.h>
-#include <fcntl.h>
+// int	main(void)
+// {
+// 	int		fd;
+// 	char	*line;
 
-int	main(void)
-{
-	int		fd;
-	char	*line;
-
-	fd = open("test.txt", O_RDONLY);
-	while (line)
-	{
-		line = get_next_line(fd);
-		printf("> %s", line);
-		free(line);
-	}
-	close (fd);
-	return (0);
-}
+// 	fd = open("./gnlTester/files/empty", O_RDONLY);
+// 	get_next_line(fd);
+// 	get_next_line(fd);
+// }
+// 	while ((line = get_next_line(fd)) != NULL)
+// 	{
+// 		printf("> %s", line);
+// 		free(line);
+// 	}
+// 	close (fd);
+// 	return (0);
+// }
 
 // __attribute__((destructor))
 // static void	destructor(void) {
